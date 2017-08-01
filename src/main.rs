2290f9ce00c0ffb0e6d50cbe33b16edde6d168bb
Ryperhun <akormanyzsolt@gmail.com>
2017-08-01 14:46:46 +0200

    Added 2D array printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+enum MapItem {
+	Wall,
+	Food,
+	Empty,
+	SnakeHead,
+	SnakePart,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+	const SIZE : usize = 5;
+    let array : [[MapItem; SIZE];SIZE] = [[MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+										  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+										  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+										  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+										  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall]];
+
+	for i in 0..array.len() {
+		for j in 0..array[i].len() {
+			print!("[{}][{}]:{:?} | ", i,j, array[i][j]);
+		}
+		println!("");
+	}
+		
 }
