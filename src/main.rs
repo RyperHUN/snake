ab3492ff31d4c86bb1163173aa8871c6581cb8b3
Ryperhun <akormanyzsolt@gmail.com>
2017-08-01 15:08:03 +0200

    Added Map class

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,51 @@ enum MapItem {
 	SnakePart,
 }
 
-const SIZE : usize = 5;
+// enum SnakeDir {
+	// Up,Left,Down, Right
+// }
+
+
+const SIZE : usize = 5;	
 type MapType = [[MapItem; SIZE];SIZE]; 	//Fixed size array - 1D array [Type, size]
-										//Dynamic array: Vec<Vec<Type>>
+											//Dynamic array: Vec<Vec<Type>>
+											
+pub struct Map {		
+	array : MapType
+}
+
+impl Map {
+	pub fn new () -> Map {
+		return Map{ array : [[MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
+						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall]]};
+	}
+	
+	pub fn draw (&self) {
+		let ref array = self.array; 
+		//let array = &self.array; //Same as the above line, just another syntax
+		for i in 0..array.len() {
+			for j in 0..array[i].len() {
+				print!("[{}][{}]:{:?} | ", i,j, array[i][j]);
+			}
+			println!("");
+		}
+	}
+}
+
+pub struct MapDrawer {
+}
+
+impl MapDrawer {
+	pub fn draw(map : Map) {
+		
+	}
+}
 
 fn main() {
+	let map = Map::new();
 	
-	//TODO better initialization
-    let array : MapType = [[MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
-						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
-						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
-						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall],
-						  [MapItem::Wall, MapItem::Food, MapItem::Empty, MapItem::SnakeHead, MapItem::Wall]];
-
-	for i in 0..array.len() {
-		for j in 0..array[i].len() {
-			print!("[{}][{}]:{:?} | ", i,j, array[i][j]);
-		}
-		println!("");
-	}
-		
+	map.draw();
 }
