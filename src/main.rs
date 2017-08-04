1ca4c8c2cbd809e1d345bf5a3c59cbce88669684
Ryperhun <akormanyzsolt@gmail.com>
2017-08-04 22:17:06 +0200

    Loaded textures

--- src/main.rs
+++ src/main.rs
@@ -442,9 +442,18 @@ use sdl2::video::WindowContext;
 use sdl2::render::Texture;
 
 pub struct TextureStorage<'r> {
-	head_right : Texture<'r>,
-	head_up : Texture<'r>,
-	head_left : Texture<'r>,
+	head_down	 : Texture<'r>,
+	head_up 	 : Texture<'r>,
+	head_left 	 : Texture<'r>,
+	head_right	 : Texture<'r>,
+	
+	body_right 	: Texture<'r>,
+	body_up 	: Texture<'r>,
+	
+	body_left_up	: Texture<'r>,
+	body_right_up 	: Texture<'r>,
+	body_left_down 	: Texture<'r>,
+	body_right_down : Texture<'r>,
 }
 
 struct ImgLoader {
@@ -456,11 +465,23 @@ impl ImgLoader {
 		let mut image = lodepng::decode24_file("snake.png").unwrap();
 		let bytes: &[u8] = image.buffer.as_ref().as_bytes();
 	
-		let head_right = ImgLoader::create_texture (bytes, &texture_creator, 0, 0);
-		let head_up = ImgLoader::create_texture (bytes, &texture_creator, 0, 1);
-		let head_left = ImgLoader::create_texture (bytes, &texture_creator, 1, 0);
+		let head_right 	= ImgLoader::create_texture (bytes, &texture_creator, 0, 0);
+		let head_up 	= ImgLoader::create_texture (bytes, &texture_creator, 0, 1);
+		let head_left 	= ImgLoader::create_texture (bytes, &texture_creator, 1, 0);
+		let head_down 	= ImgLoader::create_texture (bytes, &texture_creator, 1, 1);
 		
-		return TextureStorage{head_right : head_right, head_up : head_up, head_left :  head_left};
+		let body_right 	= ImgLoader::create_texture (bytes, &texture_creator, 0, 2);
+		let body_up 	= ImgLoader::create_texture (bytes, &texture_creator, 1, 2);
+		
+		let body_left_up 	= ImgLoader::create_texture (bytes, &texture_creator, 2, 2);
+		let body_right_up 	= ImgLoader::create_texture (bytes, &texture_creator, 2, 3);
+		let body_left_down 	= ImgLoader::create_texture (bytes, &texture_creator, 2, 1);
+		let body_right_down	= ImgLoader::create_texture (bytes, &texture_creator, 0, 3);
+		
+		return TextureStorage{	head_right : head_right, 		head_up : head_up, 				head_left :  head_left,
+								head_down : head_down, 			body_right : body_right, 		body_up : body_up,
+								body_left_up : body_left_up, 	body_right_up : body_right_up,	body_left_down : body_left_down,
+								body_right_down : body_right_down};
 	}
 	fn create_texture<'r> (img_bytes : &[u8],texture_creator : &'r TextureCreator<WindowContext>,
 	selectedi : usize, selectedj : usize) -> sdl2::render::Texture<'r> {
